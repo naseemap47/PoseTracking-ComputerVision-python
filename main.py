4f42e00b4139c869a7dd52d4febc051a21fe9017
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-27 12:11:07 +0530

    added pose landmarks and connections

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from unittest import result
 import cv2
 import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
+
+pose_draw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result = pose.process(img_rgb)
 
+    if result.pose_landmarks:
+        pose_draw.draw_landmarks(img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     cv2.imshow("Image", img)
     cv2.waitKey(1)

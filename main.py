5335a4c21b20d39cf0a3e337a2596f75bf433d3b
NASEEM A P <naseemalassampattil@gmail.com>
2022-02-27 12:25:24 +0530

    draw circle at pose landmark id

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ while True:
     result = pose.process(img_rgb)
 
     if result.pose_landmarks:
+        for id, lm in enumerate(result.pose_landmarks.landmark):
+            height, width, channel = img.shape
+            x, y = int(lm.x*width), int(lm.y*height)
+            if id==0:
+                cv2.circle(
+                    img, (x,y),
+                    8, (255,0,255),
+                    3, cv2.FILLED
+                )
         pose_draw.draw_landmarks(img, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
